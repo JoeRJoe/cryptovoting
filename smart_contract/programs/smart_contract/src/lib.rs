f15c4ce09b8e7f82e80b1a0fbfc1548f599342bc
JoeRJoe <giomin27@gmail.com>
2024-02-03 12:40:15 +0100

    Definizione account Votazione, Crowdfunding e Voto

--- smart_contract/programs/smart_contract/src/lib.rs
+++ smart_contract/programs/smart_contract/src/lib.rs
@@ -1,3 +1,5 @@
+use std::time::SystemTime;
+
 use anchor_lang::prelude::*;
 
 declare_id!("EsQM7kVd85DLBrsZSVfgjdbFM6w5VUkqbu2w5Kt2ezto");
@@ -6,10 +8,32 @@ declare_id!("EsQM7kVd85DLBrsZSVfgjdbFM6w5VUkqbu2w5Kt2ezto");
 pub mod smart_contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[account]
+pub struct Votazione {
+    pub nome: String,
+    pub descrizione: String,
+    pub crowdfunding: Option<Pubkey>,
+    pub owner: Pubkey,
+    pub attiva: bool,
+    pub data_inizio: u64,
+    pub data_scadenza: u64,
+}
+
+#[account]
+pub struct Crowdfunding {
+    pub owner: Pubkey,
+    pub totale_donato: u64,
+    pub votazione: Pubkey,
+    pub attiva: bool,
+    pub data_inizio: u64,
+    pub data_scadenza: u64,
+}
+
+#[account]
+pub struct Voto {
+    pub votazione: Pubkey,
+    pub votante: Pubkey,
+    pub data: u64,
+}
